3787a88119cb00aea35e82e0bc3eb35cab4ba0cb
lorenzoMarquesini <lorenzomarquesini@gmail.com>
2025-01-14 19:21:26 -0300

    fix db

--- db.php
+++ db.php
@@ -5,9 +5,11 @@ $user = 'u482331776_repuestosmarcu';
 $password = '2D:pbQq~a';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+    $conn = new mysqli($host, $user, $password, $dbname);
+    if ($conn->connect_error) {
+        die('Error de conexión: ' . $conn->connect_error);
+    }
+} catch (Exception $e) {
     die('Error de conexión: ' . $e->getMessage());
 }
 ?> 
