75152fd337f0e6a082772e893e31d2d8ed7cc4f2
lorenzoMarquesini <lorenzomarquesini@gmail.com>
2025-01-14 22:11:43 -0300

    sitemapTes

--- sitemap.php
+++ sitemap.php
@@ -1,10 +1,14 @@
 <?php
+header('Content-Type: application/xml; charset=utf-8');
 ob_clean(); // Clear any previous output
-header("Content-Type: application/xml; charset=UTF-8");
+
 include 'db.php';
 
+// Consulta para obtener los IDs de todos los productos
+$result = $conn->query("SELECT id FROM products");
+
+
 // Ensure no whitespace before XML declaration
-echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
 
 // Indexar la página principal
@@ -13,8 +17,6 @@ echo '    <loc>https://repuestosmarcus.com.ar/index.php</loc>' . "\n";
 echo '    <priority>1.0</priority>' . "\n";
 echo '  </url>' . "\n";
 
-// Consulta para obtener los IDs de todos los productos
-$result = $conn->query("SELECT id FROM products");
 
 // Indexar cada product.php?id= existente
 while($row = $result->fetch_assoc()) {
