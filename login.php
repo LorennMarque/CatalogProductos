dd2686a447693b39089942f3810f4ba3f0d6a424
lorenzoMarquesini <lorenzomarquesini@gmail.com>
2025-01-15 14:10:50 -0300

    UseConn

--- login.php
+++ login.php
@@ -7,9 +7,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $stmt = $pdo->prepare('SELECT * FROM users WHERE username = ?');
-    $stmt->execute([$username]);
-    $user = $stmt->fetch();
+    $stmt = $conn->prepare('SELECT * FROM users WHERE username = ?');
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
 
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
